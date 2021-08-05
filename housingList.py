87d61b71d30daeb0322e25a42cdc742ee9160fd7
Norma Sutton Harary <normalenora@gmail.com>
2021-08-05 17:49:47 -0400

    added main function

--- housingList.py
+++ housingList.py
@@ -10,9 +10,14 @@ df = pd.read_csv(filename)
 addresses = [f"{row['ADDRESS']} {row['ZIP CODE']}" for ind, row in df.iterrows()]
 developments = Counter([row['DEVELOPMENT'] for ind, row in df.iterrows()])
 dev_zip = [f"{row['DEVELOPMENT']} {row['ZIP CODE']}" for ind, row in df.iterrows()]
+unique_dev_zip = sorted(set(dev_zip))
 
 
-print(f'addresses with zipcodes = {len(addresses)}')
-print(f'developments = {len(set(developments))}')
-print(f'developments with zipcodes unique = {len(set(dev_zip))}')
+def main():
+    # print(f'addresses with zipcodes = {len(addresses)}')
+    # print(f'developments = {len(set(developments))}')
+    # print(f'developments with zipcodes unique = {len(unique_dev_zip)}')
+    [print(val) for val in unique_dev_zip]
 
+if __name__ == '__main__':
+    main()
